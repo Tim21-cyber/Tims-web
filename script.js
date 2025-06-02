e91c2fd54981f056a42feb550c6baedeffed035a
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-02 22:31:09 +0300

    adds function to elements in index.html

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function addToCart(productName) {
+    alert(productName + " has been added to your cart!");
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+    this.reset(); // Resets the form after submission
+}
+);
