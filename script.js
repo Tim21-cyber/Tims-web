1e5999587c75d031876d46a8a57ed8a5a9368135
Timothy <timothy.ochieng1@student.moringaschool.com>
2025-06-04 09:53:49 +0300

    Add smooth scroll functionality to nav links

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-function addToCart(productName) {
-    alert(productName + " has been added to your cart!");
-}
-
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Thank you for contacting us! We will get back to you soon.');
-    this.reset(); // Resets the form after submission
-}
-);
+// Javascript function: for smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                target.scrollIntoView({ behavior: 'smooth' });
+            });
+        });
